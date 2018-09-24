b31fa620c23ffab846781f392d4d28a50b3ec7c2
Amber <astronomeramber@gmail.com>
2018-09-23 22:26:30 -0700

    Added weights

--- users_cosine_similarity.py
+++ users_cosine_similarity.py
@@ -10,12 +10,16 @@ from sklearn.metrics.pairwise import cosine_similarity
 import sys
 
 gender = 'F'
-occupation = 'engineer'
+occupation = 'scientist'
 age = 25
 location = 'CA'
-if location=='CA'or'OR':
+if location=='CA':
     location = '90000'
-int(location[0])
+
+W_age = 0.25
+W_gen = 0.25
+W_job = 0.25
+W_zip = 0.25
 
 data_cols = ['user_id', 'item_id', 'rating', 'timestamp']
 item_cols = ['movie_id','movie_title','release_date', 'video_release_date','IMDb_URL','unknown','Action','Adventure','Animation','Childrens','Comedy','Crime','Documentary','Drama','Fantasy','Film-Noir','Horror','Musical','Mystery','Romance ','Sci-Fi','Thriller','War' ,'Western']
@@ -48,21 +52,21 @@ def nearest_region(x1):
 
 #print(nearest_region(location))
 
-A = pd.get_dummies(df_users['age'].apply(nearest_5years))
-B = pd.get_dummies(df_users.occupation)
-C = pd.get_dummies(df_users.gender)
-D = pd.get_dummies(df_users['zip_code'].apply(nearest_region))
+A = W_gen * pd.get_dummies(df_users.gender)
+B = W_job * pd.get_dummies(df_users.occupation)
+C = W_age * pd.get_dummies(df_users['age'].apply(nearest_5years))
+D = W_zip * pd.get_dummies(df_users['zip_code'].apply(nearest_region))
 
-df_new = pd.concat([C,A,B,D], axis = 1)
+df_new = pd.concat([A,B,C,D], axis = 1)
 
 #User information
-User_info = df_new.iloc[0].copy()
-User_info.iloc[0:] = 0
-User_info[gender] = 1
-User_info[nearest_5years(age)] = 1
-User_info[nearest_region(location)] = 1
-User_info[occupation] = 1
-#User_info.head(50)
+User_info = df_new.iloc[0].copy() #get an example user profile
+User_info.iloc[0:] = 0 #empty profile to fill with input user
+User_info[gender] = 1 * W_gen
+User_info[nearest_5years(age)] = 1 * W_age
+User_info[nearest_region(location)] = 1 * W_zip
+User_info[occupation] = 1 * W_job
+User_info.head(50)
 
 #r = np.random.randint(0,943), for random user df_new.iloc[r]
 sim=[]
@@ -70,7 +74,7 @@ for i in range(len(df_new)): #finds the
     sim.append(cosine_similarity([User_info], [df_new.iloc[i]]))
 
 item = np.argsort(sim, axis=0)[-5:]# 5 users with the highest similarity to input user
-np.argsort(sim, axis=0)[-5:]
+#np.argsort(sim, axis=0)[-5:] #similarity value (0-1)
 #np.sort(np.squeeze(sim))[-5:]
 
 item = item+1 #to get the correct indexing
@@ -82,16 +86,15 @@ U2 = df_data_sort.loc[df_data_sort['user_id'] == item[1][0][0]]
 U3 = df_data_sort.loc[df_data_sort['user_id'] == item[2][0][0]]
 U4 = df_data_sort.loc[df_data_sort['user_id'] == item[3][0][0]]
 U5 = df_data_sort.loc[df_data_sort['user_id'] == item[4][0][0]]
-#All movie IDs/recommendations from top 5 users
-
-df_top_data = pd.concat([U1,U2,U3,U4,U5],axis=0)
+#All movie IDs/recommendations from top 3 users
+df_top_data = pd.concat([U1,U2,U3],axis=0)
 df_top_data = df_top_data.sort_values('user_id', ascending=True)
 df_top_data = df_top_data[df_top_data.rating > 3] #must have 4-5 star rating
 
 top_movies_list = df_top_data['item_id'].value_counts().index.tolist()#.iloc[:5]
+df_top_data['item_id'].value_counts()
 top_movies_list = [x - 1 for x in top_movies_list] #correct indexing
-idx = top_movies_list[0:10]
+idx = top_movies_list[::]
 
-print(df_item['movie_title'].loc[idx])
+print(df_item['movie_title'].loc[idx[0:5]])
 
-#overall these 10 users are the most similar
